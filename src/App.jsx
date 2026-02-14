40d055ddd3a028581551abd3f75810935418cc63
hafizullohkomilov013-cyber <hafizullohkomilov013@gmail.com>
2026-02-14 22:42:19 +0500

    the and

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-
-import {decrement, increment} from "./redux/features/counter/counterSlice"
 
 function App() {
-  let {data} = useSelector((state) => state.counter)
-  let dispatch = useDispatch()
-  
   return (
     <div>
-      <h1>{data}</h1>
-      <button onClick={() => dispatch(increment())}>+</button>
-      <button onClick={() => dispatch(decrement())}>-</button>
+      
     </div>
   )
 }
