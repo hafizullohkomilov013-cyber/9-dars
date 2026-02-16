2377f9c8d2a0539a05d0f2475dcfe363ec051492
hafizullohkomilov013-cyber <hafizullohkomilov013@gmail.com>
2026-02-16 14:21:27 +0500

    xsx

--- src/redux/stor.js
+++ src/redux/stor.js
@@ -2,8 +2,3 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import CounterRecucer from "./features/counter/counterSlice"
 
-export const store = configureStore({
-    reducer: {
-        counter : CounterRecucer
-    }
-})
