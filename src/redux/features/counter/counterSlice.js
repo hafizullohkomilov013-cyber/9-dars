b32e87ba1bb7274818bcbfcd4a140cd6bc2dbb02
hafizullohkomilov013-cyber <hafizullohkomilov013@gmail.com>
2026-02-20 21:40:01 +0500

    the and

--- src/redux/features/counter/counterSlice.js
+++ src/redux/features/counter/counterSlice.js
@@ -1,22 +1,2 @@
-import { createSlice } from "@reduxjs/toolkit";
-
-let initialState = {
-  data: 0,
-};
-
-export const counterSlice = createSlice  ({
-    name : "counter",
-    initialState,
-    reducers:{
-        increment: (state) => {
-            state.value += 1
-        },
-        decrement: (state) => {
-            state.value -= 1
-        },
-    }
-})
-
-export const {decrement, increment} = counterSlice.actions;
 
 export default counterSlice.reducer
